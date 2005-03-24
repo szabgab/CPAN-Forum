708fbc88b5d32c3b363f9a3b6872c3d3e5676ca9
Gabor Szabo <szabgab@gmail.com>
2005-03-24 06:22:53 +0000

    more schema from DBI.pm to schema.sql
    git-svn-id: https://svn1.hostlocal.com/cpan-forum/trunk@138 e8c0bd59-b5ed-0310-baec-9ce68b663d6d

--- lib/CPAN/Forum/DBI.pm
+++ lib/CPAN/Forum/DBI.pm
@@ -23,13 +23,14 @@ $group_types{$group_types[$_]} = $_ for (0..$#group_types);
 
 # Initialize the database
 sub init_db {
-	my $class = shift;
-	my $dbfile = shift;
+	my ($class, $schema_file, $dbfile) = @_;
+
 	die "No database file supplied" if not $dbfile;
 
 	my $sql;
 	my $dbh = $class->db_Main;
-	$sql = join('', <DATA>);
+	open my $data, "<", $schema_file or die "Coult no open '$schema_file'  $!\n";
+	$sql = join('', <$data>);
 
 	for my $statement (split /;/, $sql) {
 		if ($dbh->{Driver}{Name} =~ /SQLite/) {
@@ -47,117 +48,4 @@ sub init_db {
 }
 
 1;
-__DATA__
-CREATE TABLE users (
-			id               INTEGER PRIMARY KEY auto_increment,
-			username         VARCHAR(255) UNIQUE,
-			password         VARCHAR(255),
-			email            VARCHAR(255) UNIQUE,
-			fname            VARCHAR(255),
-			lname            VARCHAR(255),
-			update_on_new_user VARCHAR(1),
-			status           INTEGER
--- registration_date
--- last_seen
--- locaton
--- user_localtime
--- scratch_pad
-
-);
-
-CREATE TABLE usergroups (
-			id               INTEGER PRIMARY KEY auto_increment,
-			name             VARCHAR(255) UNIQUE
-);
-
-CREATE TABLE user_in_group (
-			uid               INTEGER,
-			gid               INTEGER
-);
-
-CREATE TABLE configure (
-			field             VARCHAR(255),
-			value             VARCHAR(255)
-);
-
-
---CREATE TABLE grouptypes (
---			id               INTEGER PRIMARY KEY auto_increment,
---			name             VARCHAR(255) NOT NULL
---);
--- grouptypes can be   Global/Distribution/Field
-
-
-
-CREATE TABLE groups (
-			id               INTEGER PRIMARY KEY auto_increment,
-			name             VARCHAR(255) UNIQUE NOT NULL,
-			status           INTEGER,
-			gtype            INTEGER NOT NULL
-);
-
-CREATE TABLE metagroups (
-			id               INTEGER PRIMARY KEY auto_increment,
-			name             VARCHAR(255) UNIQUE NOT NULL,
-			status           INTEGER,
-);
-
-CREATE TABLE group_in_meta (
-			meta             INTEGER NOT NULL,
-			group            INTEGER NOT NULL
-			,FOREIGN KEY (meta)  REFERENCES metagroups(id)
-			,FOREIGN KEY (group) REFERENCES groups(id)
-);
-
-	
-
-CREATE TABLE grouprelations (
-			parent            INTEGER NOT NULL,
-			child            INTEGER NOT NULL
-			,FOREIGN KEY (parent) REFERENCES groups(id)
-			,FOREIGN KEY (child) REFERENCES groups(id)
-);
-
--- grouprelations defined which group belongs to which other group, 
--- In the application level we'll have to implement the restriction so 
--- Global group will have no parent
--- Fields will have Global as parent
--- Distributions will have Fields as parent one child can have several parents
--- Modules (if added) will have Distributions as parents
-
-
-CREATE TABLE posts (
-			id               INTEGER PRIMARY KEY auto_increment,
-			gid              INTEGER NOT NULL,
-			uid              INTEGER NOT NULL,
-			parent           INTEGER,
-			thread           INTEGER,
-			hidden           BOOLEAN,
-			subject          VARCHAR(255) NOT NULL,
-			text             VARCHAR(100000) NOT NULL,
-			date             TIMESTAMP
-			,FOREIGN KEY (gid) REFERENCES groups(id)
-			,FOREIGN KEY (uid) REFERENCES users(id)
-			,FOREIGN KEY (parent) REFERENCES posts(id)
-);
-
-CREATE TABLE subscriptions (
-			id               INTEGER PRIMARY KEY auto_increment,
-			uid              INTEGER NOT NULL,
-			gid              INTEGER NOT NULL,
-			allposts         BOOLEAN,
-			starters         BOOLEAN,
-			followups        BOOLEAN,
-			announcements    BOOLEAN
-			,FOREIGN KEY (gid) REFERENCES groups(id)
-			,FOREIGN KEY (uid) REFERENCES users(id)
-);
-
-CREATE TABLE sessions (
-    id               CHAR(32) NOT NULL UNIQUE,
-    a_session        TEXT NOT NULL,
-    uid              INTEGER
-);
-
-
 
---
+++ schema/schema.sql
@@ -0,0 +1,143 @@
+CREATE TABLE users (
+			id               INTEGER PRIMARY KEY auto_increment,
+			username         VARCHAR(255) UNIQUE,
+			password         VARCHAR(255),
+			email            VARCHAR(255) UNIQUE,
+			fname            VARCHAR(255),
+			lname            VARCHAR(255),
+			update_on_new_user VARCHAR(1),
+			status           INTEGER
+-- registration_date
+-- last_seen
+-- locaton
+-- user_localtime
+-- scratch_pad
+
+);
+
+CREATE TABLE usergroups (
+			id               INTEGER PRIMARY KEY auto_increment,
+			name             VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE user_in_group (
+			uid               INTEGER,
+			gid               INTEGER
+);
+
+CREATE TABLE configure (
+			field             VARCHAR(255),
+			value             VARCHAR(255)
+);
+
+
+--CREATE TABLE grouptypes (
+--			id               INTEGER PRIMARY KEY auto_increment,
+--			name             VARCHAR(255) NOT NULL
+--);
+-- grouptypes can be   Global/Distribution/Field
+
+
+
+CREATE TABLE groups (
+			id               INTEGER PRIMARY KEY auto_increment,
+			name             VARCHAR(255) UNIQUE NOT NULL,
+			status           INTEGER,
+			gtype            INTEGER NOT NULL
+);
+
+CREATE TABLE metagroups (
+			id               INTEGER PRIMARY KEY auto_increment,
+			name             VARCHAR(255) UNIQUE NOT NULL,
+			status           INTEGER
+);
+
+CREATE TABLE group_in_meta (
+			mgid             INTEGER NOT NULL,
+			gid              INTEGER NOT NULL
+			,FOREIGN KEY (mgid)  REFERENCES metagroups(id)
+			,FOREIGN KEY (gid)   REFERENCES groups(id)
+);
+
+	
+
+CREATE TABLE grouprelations (
+			parent            INTEGER NOT NULL,
+			child            INTEGER NOT NULL
+			,FOREIGN KEY (parent) REFERENCES groups(id)
+			,FOREIGN KEY (child) REFERENCES groups(id)
+);
+
+-- grouprelations defined which group belongs to which other group, 
+-- In the application level we'll have to implement the restriction so 
+-- Global group will have no parent
+-- Fields will have Global as parent
+-- Distributions will have Fields as parent one child can have several parents
+-- Modules (if added) will have Distributions as parents
+
+
+CREATE TABLE posts (
+			id               INTEGER PRIMARY KEY auto_increment,
+			gid              INTEGER NOT NULL,
+			uid              INTEGER NOT NULL,
+			parent           INTEGER,
+			thread           INTEGER,
+			hidden           BOOLEAN,
+			subject          VARCHAR(255) NOT NULL,
+			text             VARCHAR(100000) NOT NULL,
+			date             TIMESTAMP
+			,FOREIGN KEY (gid) REFERENCES groups(id)
+			,FOREIGN KEY (uid) REFERENCES users(id)
+			,FOREIGN KEY (parent) REFERENCES posts(id)
+);
+
+CREATE TABLE authors (
+			id               INTEGER PRIMARY KEY auto_increment,
+			pauseid          VARCHAR(100)
+);
+
+
+CREATE TABLE subscriptions (
+			id               INTEGER PRIMARY KEY auto_increment,
+			uid              INTEGER NOT NULL,
+			gid              INTEGER NOT NULL,
+			allposts         BOOLEAN,
+			starters         BOOLEAN,
+			followups        BOOLEAN,
+			announcements    BOOLEAN
+			,FOREIGN KEY (gid) REFERENCES groups(id)
+			,FOREIGN KEY (uid) REFERENCES users(id)
+);
+
+CREATE TABLE subscriptions_all (
+			id               INTEGER PRIMARY KEY auto_increment,
+			uid              INTEGER NOT NULL,
+			allposts         BOOLEAN,
+			starters         BOOLEAN,
+			followups        BOOLEAN,
+			announcements    BOOLEAN
+			,FOREIGN KEY (uid) REFERENCES users(id)
+);
+
+CREATE TABLE subscriptions_pauseid (
+			id               INTEGER PRIMARY KEY auto_increment,
+			uid              INTEGER NOT NULL,
+			pauseid          INTEGER NOT NULL,
+			allposts         BOOLEAN,
+			starters         BOOLEAN,
+			followups        BOOLEAN,
+			announcements    BOOLEAN
+			,FOREIGN KEY (gid) REFERENCES groups(id)
+			,FOREIGN KEY (uid) REFERENCES users(id)
+);
+
+
+
+CREATE TABLE sessions (
+    id               CHAR(32) NOT NULL UNIQUE,
+    a_session        TEXT NOT NULL,
+    uid              INTEGER
+);
+
+
+
