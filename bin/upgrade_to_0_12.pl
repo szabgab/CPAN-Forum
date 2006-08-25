724501bcf5a205eb156734ca5777b0ea81d970d0
Gabor Szabo <szabgab@gmail.com>
2006-08-25 15:15:09 +0000

    add script to do the upgrade
    git-svn-id: https://svn1.hostlocal.com/cpan-forum/trunk@166 e8c0bd59-b5ed-0310-baec-9ce68b663d6d

---
+++ bin/upgrade_to_0_12.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use lib "lib";
+
+use File::Copy qw(move copy);
+use DBI;
+use Cwd qw(cwd);
+
+# tell application db is closed...
+
+my $time   = time;
+my $failed;
+my $db_file = 'db/forum.db';
+my $backup = $db_file . "_$time";
+print "Backup: $backup\n";
+copy $db_file, $backup;
+my $dbh = DBI->connect("dbi:SQLite:dbname=$db_file") 
+    or die "Could not connect to database";
+my $source = do {
+    local $/ = undef;
+    <DATA>;
+};
+
+foreach my $sql (split /;/, $source) {
+    next if $sql !~ /\S/;
+    $dbh->do($sql);
+}
+#$failed = 1;
+
+if ($failed) {
+	print "Upgraded failed.\n";
+    unlink $db_file;
+	move $backup, $db_file;
+	print "Database restored\n";
+} else {
+	print "The datbase was upgraded successfully.\n";
+	
+}
+# set the application to "open" again
+
+__END__
+ALTER TABLE groups ADD version VARCHAR(100);
+ALTER TABLE groups ADD pauseid INTEGER;
+ALTER TABLE groups ADD rating VARCHAR(10);
+ALTER TABLE groups ADD review_count INTEGER;
+
+
