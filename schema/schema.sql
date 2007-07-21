98fbab945bfa317554a8fc659f3bb355503af672
Gabor Szabo <szabgab@gmail.com>
2007-07-21 15:16:10 +0000

    include the upgade.sql in the main schema.sql
    git-svn-id: https://svn1.hostlocal.com/cpan-forum/trunk@279 e8c0bd59-b5ed-0310-baec-9ce68b663d6d

--- schema/schema.sql
+++ schema/schema.sql
@@ -140,5 +140,24 @@ CREATE UNIQUE INDEX groups_name ON groups (name);
 CREATE INDEX posts_thread ON posts (thread);
 CREATE INDEX posts_gid ON posts (gid);
 
+---- add tagcloud
+--- TODO unique tag_id, group_id pair
+CREATE TABLE tags (
+			id               INTEGER PRIMARY KEY,
+			name             VARCHAR(100) UNIQUE NOT NULL
+);
+CREATE UNIQUE INDEX tags_name ON tags (name);
+CREATE TABLE tag_cloud (
+			uid              INTEGER,
+			tag_id           INTEGER,
+            group_id         INTEGER,
+            stamp            TEXT
+			,FOREIGN KEY (uid) REFERENCES users(id)
+			,FOREIGN KEY (tag_id) REFERENCES tags(id)
+			,FOREIGN KEY (group_id) REFERENCES groups(id)
+);
+CREATE INDEX tags_cloud_uid ON tag_cloud (uid);
+CREATE INDEX tags_cloud_tag_id ON tag_cloud (tag_id);
+CREATE INDEX tags_cloud_group_id ON tag_cloud (group_id);
 
 
