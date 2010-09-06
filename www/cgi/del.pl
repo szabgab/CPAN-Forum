13b7d44889d1f3a94151b94d6a4ef4cfe9fa5194
Gabor Szabo <szabgab@gmail.com>
2010-09-06 21:08:12 +0300

    some name update in del.pl

--- www/cgi/del.pl
+++ www/cgi/del.pl
@@ -11,14 +11,15 @@ my $name = $q->param('name');
 print $q->header;
 
 my @users = qw(nationshoes njgoogle wowpowerlvl gdoit shoesgreat jordan2008 pickcounty cnbearing);
-push @users, qw(rainyjin angelae8654 nisha jojokinkaid billy001 papu);
-push @users, qw(bettytalk mcgaha digitss adam01 mili sales3 sindabearing dpibearing yangmayer);
-push @users, qw(salesplaza releasesoft overnightmoney);
+#push @users, qw(rainyjin angelae8654 nisha jojokinkaid billy001 papu);
+#push @users, qw(bettytalk mcgaha digitss adam01 mili sales3 sindabearing dpibearing yangmayer);
+#push @users, qw(salesplaza releasesoft overnightmoney);
+push @users, qw(markettips1);
 my $sql = "delete from posts where id in (select posts.id from posts, users where username ";
 $sql .=   "in(" . join ", ", map {"'$_'"} @users;
 $sql .=  ") and posts.uid=users.id)";
-#print $sql;
-$dbh->do($sql);
+print $sql;
+print $dbh->do($sql);
 print "OK";
 
 __END__
